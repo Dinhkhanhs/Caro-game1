3238b0d09c0a78bb64766a57c05806a465d1e5a4
Thanh Truc <phamtruc6745@gmail.com>
2026-04-19 23:24:22 +0700

    Add ANSI color definitions and board symbols

--- game.h
+++ game.h
@@ -1,1 +1,11 @@
-
+// ---------Chuoi mau cho ban co ------------
+#define ANSI_RESET   "\x1B[0m"
+#define ANSI_CLEAR   "\x1B[2J\x1B[H"
+#define ANSI_RED     "\x1B[91m"
+#define ANSI_CYAN    "\x1B[96m"
+#define ANSI_GRAY    "\x1B[90m"
+#define ANSI_YELLOW  "\x1B[93m" 
+//------- ky tu de tao hinh dang ban co--------
+#define H_LINE "\xE2\x94\x80"
+#define V_LINE "\xE2\x94\x82"
+#define CROSS  "\xE2\x94\xBC"
