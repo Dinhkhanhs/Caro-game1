a448e52ac99f8c7ad83cc67ea9386075a036afe5
Thanh Truc <phamtruc6745@gmail.com>
2026-04-21 00:03:54 +0700

    Update console.c

--- console.c
+++ console.c
@@ -7,7 +7,7 @@
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
 #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 #endif
-
+// day la ham giup console hieu duoc cac ma dieu khien nhu ma mau sac, di chuyen con tro,....
 void setupWindowsConsole() {
     SetConsoleOutputCP(CP_UTF8); 
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);// day la chia khoa de mo ra quyen can thiep vao console
