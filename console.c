3e5f55c777b7e69532a409b02800146865c2550c
Thanh Truc <phamtruc6745@gmail.com>
2026-04-21 00:02:33 +0700

    Enhance console setup function with comments
    Added comments to clarify the purpose of the code.

--- console.c
+++ console.c
@@ -10,10 +10,10 @@
 
 void setupWindowsConsole() {
     SetConsoleOutputCP(CP_UTF8); 
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hOut != INVALID_HANDLE_VALUE) {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);// day la chia khoa de mo ra quyen can thiep vao console
+    if (hOut != INVALID_HANDLE_VALUE) { // kiem tra xem co du dieu kien de cap chia khoa chua 
         DWORD dwMode = 0;
-        if (GetConsoleMode(hOut, &dwMode)) {
+        if (GetConsoleMode(hOut, &dwMode)) { // ktr xem he thong da cho phep doc cau hinh hien tai cua console 
             dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
             SetConsoleMode(hOut, dwMode); 
         }
