8fb1170af8497f5d7d6533ebae839b47c22e887d
Dinhkhanhs <duongdinhkhanh2007@gmail.com>
2026-04-28 22:24:13 +0700

    Update game.h with new structures and functions
    Refactor game.h to improve structure and add new functionalities.

--- game.h
+++ game.h
@@ -2,24 +2,19 @@
 #define GAME_H
 
 #include <stdbool.h>
-#include <cau_truc_du_lieu.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// --- CAU HINH GAME MAC DINH ---
+// --- CAU HINH ---
 #define BOARD_SIZE 15
+#define WIN_COUNT 5
 
-// --- CHUOI THOAT ANSI ---
-#define ANSI_RESET   "\x1B[0m"
-#define ANSI_CLEAR   "\x1B[2J\x1B[H"
-#define ANSI_RED     "\x1B[91m"
-#define ANSI_CYAN    "\x1B[96m"
-#define ANSI_GRAY    "\x1B[90m"
-#define ANSI_YELLOW  "\x1B[93m" 
-#define ANSI_BG_DARK "\x1B[100m"
-
-// --- KY TU VE HOP ---
-#define H_LINE "\xE2\x94\x80"
-#define V_LINE "\xE2\x94\x82"
-#define CROSS  "\xE2\x94\xBC"
+// Thong nhat dung char de de hien thi
+typedef char Stone;
+#define STONE_X 'X'
+#define STONE_O 'O'
+#define STONE_NA ' '
 
 // --- CAU TRUC DU LIEU ---
 typedef struct {
@@ -29,32 +24,35 @@ typedef struct {
 } Move;
 
 typedef struct {
+    Move data[BOARD_SIZE * BOARD_SIZE];
+    int top;
+} Stack;
+
+typedef struct {
     int blackWins;
     int whiteWins;
     int draws;
 } ScoreBoard;
 
-// Thêm vào sau struct
-extern Stone board[BOARD_SIZE][BOARD_SIZE];
-extern ScoreBoard gameScore;
+// --- MA MAU ANSI ---
+#define ANSI_RESET   "\x1B[0m"
+#define ANSI_CLEAR   "\x1B[2J\x1B[H"
+#define ANSI_RED     "\x1B[91m"
+#define ANSI_CYAN    "\x1B[96m"
+#define ANSI_YELLOW  "\x1B[93m"
+#define ANSI_GRAY    "\x1B[90m"
 
-// --- KHAI BAO NGUYEN MAU HAM (API) ---
-
-// Console
+// --- KHAI BAO HAM ---
 void setupWindowsConsole();
-int showSplashScreen(); 
+int showSplashScreen();
+void initBoard(Stone board[BOARD_SIZE][BOARD_SIZE], int size);
+void displayBoard(Stone board[BOARD_SIZE][BOARD_SIZE], int size, int cursorRow, int cursorCol);
+bool checkWinner(Stone board[BOARD_SIZE][BOARD_SIZE], int size, int r, int c, Stone player);
+void drawStatusBar(int cursorX, int cursorY, int timeRemaining);
 
-// Ban co (Da them tham so int size)
-void initBoard(char board[BOARD_SIZE][BOARD_SIZE], int size);
-void displayBoard(char board[BOARD_SIZE][BOARD_SIZE], int size, int cursorRow, int cursorCol);
+// Stack functions
+void initStack(Stack *s);
+bool pushMove(Stack *s, Move m);
+bool popMove(Stack *s, Move *m);
 
-// Logic (Da them tham so int size)
-int countDirection(char board[BOARD_SIZE][BOARD_SIZE], int size, int r, int c, int dr, int dc, Stone player);
-bool checkWinner(char board[BOARD_SIZE][BOARD_SIZE], int size, int lastRow, int lastCol, Stone player);
-
-// Diem so
-void loadScore(ScoreBoard *sb);
-void saveScore(ScoreBoard sb);
-void updateScore(ScoreBoard *sb, Stone winner);
-
-#endif // GAME_H
+#endif
